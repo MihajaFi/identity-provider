fd29d9666de5f4b9d8a8ecbef535dd7643ae1f29
MihajaFi <hei.fifaliana@gmail.com>
2025-01-13 22:43:09 +0300

    fix: change static file to dinamic

--- src/main/java/school/hei/identity/config/FirebaseConfig.java
+++ src/main/java/school/hei/identity/config/FirebaseConfig.java
@@ -6,7 +6,7 @@ import com.google.firebase.FirebaseOptions;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
-import java.io.FileInputStream;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
 @Configuration
@@ -14,7 +14,13 @@ public class FirebaseConfig {
 
     @PostConstruct
     public void init() throws IOException {
-        FileInputStream serviceAccount = new FileInputStream("src/main/resources/firebase/google-services.json");
+        String firebaseConfig = System.getenv("GOOGLE_SERVICES");
+
+        if (firebaseConfig == null || firebaseConfig.isEmpty()) {
+            throw new IllegalStateException("La variable d'environnement GOOGLE_SERVICES_JSON n'est pas définie !");
+        }
+
+        ByteArrayInputStream serviceAccount = new ByteArrayInputStream(firebaseConfig.getBytes());
 
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
